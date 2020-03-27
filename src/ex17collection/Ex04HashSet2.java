642c5ce48d6ea89e19be1a016509c60218ec451d
Kosmo_04 <Kosmo_04@DESKTOP-G07SS9U>
2020-03-27 18:28:08 +0900

    03-27

--- src/ex17collection/Ex04HashSet2.java
+++ src/ex17collection/Ex04HashSet2.java
@@ -12,11 +12,17 @@ public class Ex04HashSet2 {
 		Person p1 = new Person("현빈", 30);
 		Person p2 = new Person("현빈짱", 20);
 		Person p3 = new Person("현빈", 30);
+		Person p4 = new Person("현빈", 40);
+		Person p5 = new Person("현빈짱짱", 30);
 		
 		hashSet.add(p1);
 		hashSet.add(p2);
 		boolean b = hashSet.add(p3);
+		boolean c = hashSet.add(p4);
+		boolean d = hashSet.add(p5);
 		System.out.println("p3 객체 저장여부: "+ b);
+		System.out.println("p3 객체 저장여부: "+ c);
+		System.out.println("p3 객체 저장여부: "+ d);
 		
 		System.out.println("HashSet에 저장된 객체수:"+ hashSet.size());
 		
